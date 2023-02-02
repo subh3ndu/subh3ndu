5ed8139ccd0c9d7068f6796d6fa7d7a1827e754b
Subhendu Karmakar <subhan@Subhendus-MacBook-Air.local>
2023-02-02 18:27:25 +0530

    test

---
+++ a.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+// 
+
+void solve() {
+    
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int tc=1;
+    cin >> tc;
+    while(tc--) {
+        solve();
+    }
+    
+    return 0;
+}
